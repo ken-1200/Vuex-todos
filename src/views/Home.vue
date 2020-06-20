60b014020142972c2709ad39eb0e983c3abd60c0
Kensuke Machida <ken@MacBook-Pro.local>
2020-06-21 01:40:24 +0900

    [add]'v-forでstateの配列を表示'

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,22 @@
 <template>
   <div>
-    <h2>Home</h2>
+    <h2>Todo List</h2>
+    <ul class="todo-list">
+      <li v-for="todo in todos" :key="todo.id" class="todo-list__items">
+        <p class="todo-list__title">{{ todo.title }}</p>
+      </li>
+    </ul>
+    <router-link to="/add">＋</router-link>
   </div>
 </template>
 
 <script>
 export default {
-  
+  computed: {
+    todos() {
+      return this.$store.state.todoData
+    }
+  }
 }
 </script>
 

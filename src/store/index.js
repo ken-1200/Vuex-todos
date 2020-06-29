5817ed26d731b028cd917d5ad314646131a316e4
Kensuke Machida <ken@MacBook-Pro.local>
2020-06-29 09:02:55 +0900

    [add]'Countするgetters'

--- src/store/index.js
+++ src/store/index.js
@@ -14,8 +14,11 @@ export default new Vuex.Store({
     // タスクが完了していないリスト
     notDoneTodos: state => state.todoData.filter(todo => todo.done != true),
     // 全体のタスク数
-    // タスクが完了したリスト数
+    allTodosCount: state => state.todoData.length,
+    // タスクが完了したリスト数...(state, 他のgetters)
+    doneTodosCount: (state, getters) => getters.doneTodos.length,
     // タスクが完了していないリスト数
+    notDoneTodosCount: (state, getters) => getters.notDoneTodos.length,
     // idに該当するタスクの取得
     getTodoById: state => id => {
       return state.todoData.find(todo => todo.id === id)

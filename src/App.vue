4e226c769847f31a95ff596418e50435bda278f1
Kensuke Machida <ken@MacBook-Pro.local>
2020-06-29 09:01:03 +0900

    [sytle]'position fixed他'

--- src/App.vue
+++ src/App.vue
@@ -24,18 +24,35 @@ export default {
 
 <style lang="scss">
 header {
+  width: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  background-color: white;
+  z-index: 1100;
+
   h1 {
     border-bottom: solid 3px $cVueGreen;
     cursor: pointer;
+    margin-bottom: 0;
   }
 }
 
 main {
-  margin-left: 10px;
-  
+  padding: 80px 0;
+
   h2 {
+    width: 100%;
+    position: fixed;
+    top: 0;
+    left: 0;
+    transform: translate(10px, 60px);
     color: $cSubBlack;
     border-bottom: solid 3px $cVueGreen;
+    margin: 0;
+    padding: 20px 0 0;
+    background-color: white;
+    z-index: 1000;
   }
 }
 </style>
